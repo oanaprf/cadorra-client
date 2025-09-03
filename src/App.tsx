e688c4617b2e443394352a1860b299c7dd401af1
Oana <oannna1234@gmail.com>
2025-09-03 13:01:01 +0300

    first-commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen min-w-screen bg-[radial-gradient(circle_at_35%_8%,_#2ad0ca80_0%,_#a78bfa80_23%,_#feb0fe80_47%,_#abb3fc80_69%,_#5df7a480_88%,_#58c4f680_100%)] px-20 py-10">
+      <span className="text-7xl text-white">Cadorra</span>
+    </div>
+  );
+}
+
+export default App;
